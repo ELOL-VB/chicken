0ba4b6a73ba4eea10bfb1a625547d874cd7a1a7a
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-10 21:53:16 +0800

    Add files via upload

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim c, a, b As Integer
+        Randomize()
+        For c = 1 To 20
+            For a = 1 To 33
+                For b = 1 To 100
+                    If a + c + b = 100 And 5 * c + 3 * a + 1 / 3 * b = 100 Then
+                        Console.Write("公雞" & c & " " & "母雞" & a & " " & "小雞" & b)
+                        Console.Read()
+                    End If
+                Next
+            Next
+        Next
+
+        If a + c + b = 100 Then
+
+        End If
+
+
+    End Sub
+
+End Module
